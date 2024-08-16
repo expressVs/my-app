a2dd8cf88b0db5ebf9d612fdabc1f34d649b7c7b
OceanPowerY <1250123861@qq.com>
2024-08-16 20:42:08 +0800

    useCallback的应用场景

--- src/App.js
+++ src/App.js
@@ -1,66 +1,47 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 
-function getCount (count) {
+function Bpp(props) {
 
-    console.log('执行昂贵的计算');
+  const { click } = props
 
-    console.time('计算耗时');
+  console.log('bpp 执行')
 
-    let result = 0;
-    for (let i = 0; i < count * 1000000000; i++) {
-        result += i;
-    }
-    console.timeEnd('计算耗时');
-    return result;
+  return (
+    <h1 onClick={click}>bpp</h1>
+  )
 }
 
-const ExampleComponent = () => {
-    const [count, setCount] = useState(0);
-
-    console.log('ExampleComponent render');
-
-    // const expensiveCalculation = getCount(count);
-
-    const expensiveCalculation = useMemo(() => {
-        console.log('执行昂贵的计算');
-
-        console.time('计算耗时');
-
-        let result = 0;
-        for (let i = 0; i < count * 100000000; i++) {
-            result += i;
-        }
-        console.timeEnd('计算耗时');
-        return result;
-    }, [count]);
-
-    const incrementCount = () => {
-        setCount(prevCount => prevCount + 1);
-    };
-
-    const dec = () => {
-        setCount(prevCount => prevCount - 1);
-    }
-
-    return (
-        <div>
-            <p>Count: {count}</p>
-            <p>Expensive Calculation: {expensiveCalculation}</p>
-            <button onClick={incrementCount}>Increment</button>
-            <button onClick={dec}>dec</button>
-        </div>
-    );
-};
+// 将组件进行渲染优化
+// const BppMemo = React.memo(Bpp)
 
 function App() {
-    const [count, setCount] = useState(0);
-    return (
-        <div className="App">
-            <button onClick={() => setCount(count + 1)}>加加+++</button>
-            {count}
-            <ExampleComponent />
-        </div>
-    ); 
+  const [count, setCount] = useState(0);
+
+  // console.log('app 执行')
+
+  
+  // function click () {
+  //   alert(1)
+  // }
+
+  // let data = {}
+
+  let data = useMemo(() => {
+    return {}
+  }, [])
+
+  const click = useCallback(() => {
+    alert(1)
+  }, [])
+
+  return (
+    <div className="App">
+      <button onClick={() => setCount(count + 1)}>加加+++</button>
+      {count}
+      {/* 对click 和 data 进行缓存 需结合React.memo() */}
+      <Bpp click={click} data={data} />
+    </div>
+  );
 }
 
 export default App;
